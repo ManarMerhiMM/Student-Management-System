812862dcb0dd3b3579a0ecfc9ebe88a5c8c45966
ManarMerhiMM <manarmerhi80@gmail.com>
2025-05-24 14:56:23 +0300

    User manual done, added a link to download it in the landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,10 +6,11 @@
     <div class="text-center mt-5">
         <h1 class="display-5">Welcome to our Student Management System</h1>
         <p class="lead mt-3">Manage students, courses, and grades all in one place.</p>
+        <a class="btn btn-primary btn-lg mt-4" href="{{ asset('documents/User Manual.docx') }}" download><i class="bi bi-download me-1"></i> User Manual</a>
         @auth
-        <a href="{{ route('dashboard') }}" class="btn btn-primary btn-lg mt-4">Go to Dashboard</a>
+            <a href="{{ route('dashboard') }}" class="btn btn-primary btn-lg mt-4">Go to Dashboard</a>
         @else
-        <a href="{{ route('login') }}" class="btn btn-primary btn-lg mt-4">Login to get started</a>
+            <a href="{{ route('login') }}" class="btn btn-primary btn-lg mt-4">Login to get started</a>
         @endauth
     </div>
 @endsection
