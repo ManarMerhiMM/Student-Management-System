439041e6f3acb3532c5ab74dd0c8630fcbb46641
ManarMerhiMM <manarmerhi80@gmail.com>
2025-05-25 13:55:37 +0300

    Documentation done, added a link to download it in the landing page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
         <h1 class="display-5">Welcome to our Student Management System</h1>
         <p class="lead mt-3">Manage students, courses, and grades all in one place.</p>
         <a class="btn btn-primary btn-lg mt-4" href="{{ asset('documents/User Manual.docx') }}" download><i class="bi bi-download me-1"></i> User Manual</a>
+        <a class="btn btn-primary btn-lg mt-4" href="{{ asset('documents/Documentation.docx') }}" download><i class="bi bi-download me-1"></i> Documentation</a>
         @auth
             <a href="{{ route('dashboard') }}" class="btn btn-primary btn-lg mt-4">Go to Dashboard</a>
         @else
